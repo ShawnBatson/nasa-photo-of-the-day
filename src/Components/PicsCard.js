978b90f276c53488f165d147e863a1b78d21de98
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-09 22:42:05 -0600

    added image, styling in css, styling in styled.css as well

--- src/Components/PicsCard.js
+++ src/Components/PicsCard.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 
 const PicsCard = props => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <h6>{props.date}</h6>
-      <h5>{props.explanation}</h5>
-      <img src={props.url} alt="nasa second" />
+    <div className="cardStock">
+      <h1 className="title">{props.title}</h1>
+      <h6 className="date">{props.date}</h6>
+      <h5 className="explanation">{props.explanation}</h5>
+      <div className="image">
+        <img className="imageStock" src={props.url} alt="nasa second" />
+      </div>
     </div>
   );
 };
